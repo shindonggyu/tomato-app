e09989078594874f7c92f38c90f2e28a45c068a9
좌 준협 <jwajunhyeob@jwaui-MacBook-Air.local>
2018-04-02 22:50:52 +0900

    component 1/2

---
+++ components/timer/index.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { LinearGradient } from 'expo';
+
+
+export default class Timer extends React.Component { 
+    render(){
+        return(
+            
+            <View style={styles.container}>
+                <StatusBar
+                    barStyle="light-content"
+                />
+                <LinearGradient style={styles.linear} colors={["#f21818", "#ea7272"]} >
+                <View style={styles.upper}>
+                    <Text style={styles.time}>25:00</Text>
+                </View>
+                <View style={styles.lower}>
+                    <Text>Button here!</Text>
+                </View>
+                </LinearGradient>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+    },
+    linear: {
+        flex:1
+    },
+    upper: {
+        flex:2,
+        justifyContent: "center",
+        alignItems: "center"
+    },
+    lower: {
+        flex:1,
+        justifyContent:"center",
+        alignItems:"center"
+    },
+    time: {
+        color: "white",
+        fontSize: 100,
+        fontWeight: "200"
+    }
+})
